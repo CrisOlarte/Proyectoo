b13cbc49c474c5fd973d1a27629e7e7b5893d6af
CrisOlarte <olarthecristian@gmail.com>
2025-06-04 19:07:19 -0500

    jjj

--- src/main/java/com/usta/proyectoo/handler/LoginSuccessHandler.java
+++ src/main/java/com/usta/proyectoo/handler/LoginSuccessHandler.java
@@ -4,12 +4,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
 import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 import java.io.IOException;
+import java.util.Collection;
 
 @Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -20,11 +22,32 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
                                         Authentication authentication)
             throws IOException, ServletException {
 
+        // Flash message
         SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
         FlashMap flashMap = new FlashMap();
         flashMap.put("success", "Has iniciado sesión exitosamente " + authentication.getName());
         flashMapManager.saveOutputFlashMap(flashMap, request, response);
 
-        super.onAuthenticationSuccess(request, response, authentication);
+        // Redirección por rol
+        String redirectUrl = "/";
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (GrantedAuthority authority : authorities) {
+            String role = authority.getAuthority();
+
+            if (role.equals("ROLE_ADMIN")) {
+                redirectUrl = "/dashboard/administrativo";
+                break;
+            } else if (role.equals("ROLE_EMPRENDEDOR")) {
+                redirectUrl = "/dashboard/estudiante";
+                break;
+            } else if (role.equals("ROLE_EVALUADOR")) {
+                redirectUrl = "/dashboard/evaluador";
+                break;
+            }
+        }
+
+        // Redirigir a la URL correspondiente
+        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 }
